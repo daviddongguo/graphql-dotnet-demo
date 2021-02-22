da9368dba951cb8680d8ca58215b5c6a1ef078ce
daviddongguo <david.dong.guo@gmail.com>
2021-02-21 23:07:36 -0500

    Add Query

--- Startup.cs
+++ Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CommanderGQL.Data;
+using CommanderGQL.GraphQL;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -31,6 +32,8 @@ namespace CommanderGQL
                 Configuration.GetConnectionString("CommandConStr")
             ));
 
+            services.AddGraphQLServer().AddQueryType<Query>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,10 +48,11 @@ namespace CommanderGQL
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapGraphQL();
+                // endpoints.MapGet("/", async context =>
+                // {
+                //     await context.Response.WriteAsync("Hello World!");
+                // });
             });
         }
     }
